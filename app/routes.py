be0f2235725e6323e5255fb410ea516fb41e471a
Damian Williams <ctf_dai_lafing@msn.com>
2025-08-09 12:12:29 +0100

    Update so the homepage shows the rooms from the sqlite db file

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
+from .models import Room
 
 bp = Blueprint("main", __name__)
 
 @bp.route("/")
 def index():
-    return render_template("index.html")
+    rooms = Room.query.all()
+    return render_template("index.html", rooms=rooms)
