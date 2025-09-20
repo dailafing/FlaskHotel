3e31b8309e1f70ac5f4f6655cafa3f5ab4e4010e
Damian Williams <ctf_dai_lafing@msn.com>
2025-09-20 09:00:24 +0100

    Upgrade the run.py to alloy for local and PythonAnywhere deployment

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import create_app
 
-app = create_app()
+application = create_app()  # for WSGI (PythonAnywhere)
+app = application            # for local use
 
 if __name__ == "__main__":
     app.run(debug=True)
