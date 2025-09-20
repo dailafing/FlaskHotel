66dc2fa67a5c87e0156ebde84d0b94b4c75f0c13
Damian Williams <ctf_dai_lafing@msn.com>
2025-09-20 14:22:08 +0100

    Clean up commented code

--- app/__init__.py
+++ app/__init__.py
@@ -18,18 +18,7 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
     os.makedirs(app.instance_path, exist_ok=True)
 
-    # when i migrate, i'll use this
-    #export DATABASE_URL="mysql+pymysql://USER:PASSWORD@HOST:3306/DBNAME"
-
-    # default_sqlite = "sqlite:///" + os.path.join(app.instance_path, "hotel.db")
-    # app.config.update(
-    #     SECRET_KEY=os.environ.get("SECRET_KEY", "dev"),
-    #     SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL", default_sqlite),
-    #     SQLALCHEMY_TRACK_MODIFICATIONS=False,
-    # )
-
     app.config.from_object('instance.config.Config')
-
 
     db.init_app(app)
     login_mgr.init_app(app)
